49803cc46e2b72e7aa53d18c90f64cf7eea3324d
Selenay <selenaykeskinm@gmail.com>
2024-08-09 16:44:38 +0300

    Add fe app files

---
+++ contactapp/src/components/Contact.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { deleteContact } from '../api/ContactService';
+
+const Contact = ({ contact, onDelete }) => {
+  const handleDelete = async (e) => {
+    e.stopPropagation()
+        e.preventDefault()
+    try {
+      await deleteContact(contact.id);
+      onDelete();
+    } catch (error) {
+      console.log('Failed to delete contact:', error);
+    }
+  };
+
+  return (
+    <Link to={`/contacts/${contact.id}`} className="contact__item">
+      <div className="delete_item">
+        <button className="delete_btn" onClick={handleDelete}>
+          <i className="bi bi-trash" style={{ fontSize: '24px' }}></i>
+        </button>
+      </div>
+
+      <div className="contact__header">
+        <div className="contact__image">
+          <img src={contact.photoUrl} alt={contact.name} />
+        </div>
+        <div className="contact__details">
+          <p className="contact_name">{contact.name.substring(0, 15)}</p>
+          <p className="contact_title">{contact.title}</p>
+        </div>
+      </div>
+
+      <div className="contact__body">
+        <p>
+          <i className="bi bi-envelope"></i> {contact.email.substring(0, 15)}
+        </p>
+        <p>
+          <i className="bi bi-geo"></i> {contact.address}
+        </p>
+        <p>
+          <i className="bi bi-telephone"></i> {contact.phone}
+        </p>
+        <p>
+          {contact.status === 'Active' ? (
+            <i className="bi bi-check-circle"></i>
+          ) : (
+            <i className="bi bi-x-circle"></i>
+          )}{' '}
+          {contact.status}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Contact;
